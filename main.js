26ee8dcd94db9d6eb4f6ab3987641e9ff2650d25
Shaik Mohammed Huzaifa <shaikmohdhuz@gmail.com>
2021-12-18 23:16:47 +0530

    Added a function to calculate the Vw

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // First lets Take the input and Store into a variable
-
-let InputValue = document.getElementById("VW").InnerHTML;
+/*
+let InputValue = document.getElementById("VW").innerHTML;
 
 // Before everthing lets Create a Clear function to clear the result
 function Clear(){
-    document.getElementById().InnerHTML = '';
-    document.getElementById().InnerHTML = '';
-    document.getElementById().InnerHTML = '';
-    document.getElementById().InnerHTML = '';
+    document.getElementById().innerHTML = '';
+    document.getElementById().innerHTML = '';
+    document.getElementById().innerHTML = '';
+    document.getElementById().innerHTML = '';
 }
 
 // Lets first convert it to Vw under a function
@@ -15,15 +15,33 @@ function Clear(){
 function Px_to_ViewPortWidth(){
   // Here the product is stored in a varible so that can be displayed in DOM  
 let $Px_to_viewportWidthValue = InputValue * 0.24271844660194175;
-document.getElementById("PxtoVWAnswer").InnerHTML = $Px_to_viewportWidthValue;
+document.getElementById("PxtoVWAnswer").innerHTML = $Px_to_viewportWidthValue;
 }
 
 
 // Function for ViewPort Height 
 function Px_to_ViewPortHeight(){
 // Here the product is stored in a varible so that can be displayed in DOM  
-let $Px_to_viewportHeightValue = InputValue * 0.24271844660194175;
-document.getElementById("PxtoVHAnswer").InnerHTML = $Px_to_viewportHeightValue;
+let $Px_to_viewportHeightValue = InputValue * 0.;
+document.getElementById("PxtoVHAnswer").innerHTML = $Px_to_viewportHeightValue;
+}
+*/
+
+
+// Example to test the innerWidth for ViewPort Width
+function input(){
+    let input = document.getElementById("Px1").value;
+    console.log(input);
+    let vve = window.innerWidth;
+    let VVww = vve / 100 * input;
+   document.getElementById("h1").innerHTML = VVww; 
+      console.log(vve);
 }
 
+console.log(window.innerWidth);
 
+
+function Clear(){
+  document.getElementById("Px1").value = '';
+  document.getElementById("h1").innerHTML = '';
+}
