1b83e693269e49e407ea8438fe45723a841d1e72
Shaik Mohammed Huzaifa <shaikmohdhuz@gmail.com>
2021-12-18 19:14:34 +0530

    Added a Px to Viewport Height function to calculate the viewport height

--- main.js
+++ main.js
@@ -12,9 +12,18 @@ function Clear(){
 
 // Lets first convert it to Vw under a function
 // Function for ViewPort Width
-function ViewPortWidth(){
+function Px_to_ViewPortWidth(){
   // Here the product is stored in a varible so that can be displayed in DOM  
-let $viewportWidthValue = InputValue * 0.24271844660194175;
-document.getElementById("VWAnswer").InnerHTML = $viewportWidthValue;
+let $Px_to_viewportWidthValue = InputValue * 0.24271844660194175;
+document.getElementById("PxtoVWAnswer").InnerHTML = $Px_to_viewportWidthValue;
 }
+
+
+// Function for ViewPort Height 
+function Px_to_ViewPortHeight(){
+// Here the product is stored in a varible so that can be displayed in DOM  
+let $Px_to_viewportHeightValue = InputValue * 0.24271844660194175;
+document.getElementById("PxtoVHAnswer").InnerHTML = $Px_to_viewportHeightValue;
+}
+
 
