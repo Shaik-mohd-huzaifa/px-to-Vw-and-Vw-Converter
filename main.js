a487cb258277dca6d9721705325ce1c3262d06e7
Shaik Mohammed Huzaifa <shaikmohdhuz@gmail.com>
2022-01-12 16:09:49 +0530

    Removed the unwanted code

--- main.js
+++ main.js
@@ -1,45 +1,4 @@
-// First lets Take the input and Store into a variable
-/*
-let InputValue = document.getElementById("VW").innerHTML;
 
-// Before everthing lets Create a Clear function to clear the result
-function Clear(){
-    document.getElementById().innerHTML = '';
-    document.getElementById().innerHTML = '';
-    document.getElementById().innerHTML = '';
-    document.getElementById().innerHTML = '';
-}
-
-// Lets first convert it to Vw under a function
-// Function for ViewPort Width
-function Px_to_ViewPortWidth(){
-  // Here the product is stored in a varible so that can be displayed in DOM  
-let $Px_to_viewportWidthValue = InputValue * 0.24271844660194175;
-document.getElementById("PxtoVWAnswer").innerHTML = $Px_to_viewportWidthValue;
-}
-
-
-// Function for ViewPort Height 
-function Px_to_ViewPortHeight(){
-// Here the product is stored in a varible so that can be displayed in DOM  
-let $Px_to_viewportHeightValue = InputValue * 0.;
-document.getElementById("PxtoVHAnswer").innerHTML = $Px_to_viewportHeightValue;
-}
-
-
-// Example to test the innerWidth for ViewPort Width
-function input(){
-    let input = document.getElementById("Px1").value;
-    console.log(input);
-    let vve = window.innerWidth;
-    let VVww = vve / 100 * input;
-   document.getElementById("h1").innerHTML = VVww; 
-      console.log(vve);
-}
-
-console.log(window.innerWidth);
-
-*/
 // This clears the input and output 
 
 function Clear(){ 
@@ -62,12 +21,7 @@ function Clear(){
      document.getElementById("PxtoVh").value = '';
     }
 }
-/*
-function Clearinfo(){
-  document.getElementById("Screen").innerHTML = "";
-  document.getElementById("Screenheightinfo").innerHTML = "";
-}
-*/
+
 
  // It is declared to store the selected device weight
 let DeviceWidth;
@@ -284,8 +238,3 @@ function Choice(){
       document.getElementById('MistakeDisplay').innerHTML = "Enter the Vh units";
     }
   }
-/*
-  function color1(){
-    document.getElementById('select').style.backgroundColor = "blue";
-  }
-  */
